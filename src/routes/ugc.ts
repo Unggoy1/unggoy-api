5c454a43fad502eb78266920a77b87c0f0e19c94
Christian Vaughn <me@christianvaughn.net>
2023-12-13 15:26:22 -0800

    add maps route

---
+++ src/routes/ugc.ts
@@ -0,0 +1,29 @@
+import Elysia from "elysia";
+
+export const maps = new Elysia().get(
+  "/maps/:assetId",
+  async ({ params: { assetId } }) => {
+    const ugcEndpoint =
+      "https://discovery-infiniteugc.svc.halowaypoint.com/hi/maps/ef006625-5780-4d94-abf5-67ec0236a453/" +
+      assetId;
+    const ugcEndpointTemp =
+      "https://www.halowaypoint.com/_next/data/l_iVqOweZfILKOKRoCSTz/en-us/halo-infinite/ugc/maps/" +
+      assetId +
+      ".json";
+
+    try {
+      const response = await fetch(ugcEndpointTemp);
+
+      if (!response.ok) {
+        throw new Error(`failed to fetch data. Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      const res = data.pageProps.asset;
+      return res;
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      throw error;
+    }
+  },
+);
