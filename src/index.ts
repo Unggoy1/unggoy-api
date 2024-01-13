c12a965aeba81f05f893e6871d02b3c64fc69c1a
Christian Vaughn <me@christianvaughn.net>
2024-01-12 19:53:51 -0800

    try cors fix gain

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 const app = new Elysia()
   .use(
     cors({
-      origin: "unggoy.xyz", //TODO properly fix this and use ENV or replace this entirely
+      origin: "www.unggoy.xyz", //TODO properly fix this and use ENV or replace this entirely
     }),
   )
   .get("/", () => "Hello Elysia")
