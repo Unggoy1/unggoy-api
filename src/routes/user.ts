d5593d0bfa853ae2e357ed74536d637c2e70da6f
Christian Vaughn <me@christianvaughn.net>
2024-07-15 19:10:21 -0700

    Update user endpoint to list users playlist names and ids

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,10 +1,24 @@
 import Elysia from "elysia";
 import { authApp } from "../middleware";
+import { prisma } from "../prisma";
 export const user = new Elysia().use(authApp).get("/user", async (context) => {
   if (!context.user) {
     return new Response(null, {
       status: 401,
     });
   }
-  return context.user;
+  const user = await prisma.user.findUnique({
+    where: {
+      id: context.user.id,
+    },
+    include: {
+      Playlist: {
+        select: {
+          id: true,
+          name: true,
+        },
+      },
+    },
+  });
+  return user;
 });
