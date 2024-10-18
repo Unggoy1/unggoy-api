69acaa3b0a2b51bd107b98a508e662274ba5707d
Christian Vaughn <me@christianvaughn.net>
2024-10-18 16:31:23 -0700

    Add action to delete user and all their stuffs

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,8 +1,15 @@
 import { Elysia } from "elysia";
 import { authApp } from "../middleware";
 import prisma from "../prisma";
-import { Unauthorized, NotFound, TooManyRequests } from "../lib/errors";
+import {
+  Unauthorized,
+  NotFound,
+  TooManyRequests,
+  Forbidden,
+  Unknown,
+} from "../lib/errors";
 import { rateLimit } from "elysia-rate-limit";
+import { deleteFromS3, extractS3Key } from "../lib/imageTools";
 export const user = new Elysia()
   .use(
     rateLimit({
@@ -34,4 +41,47 @@ export const user = new Elysia()
     }
     context.set.headers["Cache-Control"] = "private, no-store, max-age=0";
     return user;
+  })
+  .delete("/user", async (context) => {
+    if (!context.user) {
+      throw new Unauthorized();
+    }
+    const user = await prisma.user.findUnique({
+      where: {
+        id: context.user.id,
+      },
+      include: {
+        Playlist: {
+          select: {
+            assetId: true,
+            thumbnailUrl: true,
+          },
+        },
+      },
+    });
+    if (!user) {
+      throw new NotFound();
+    }
+    if (context.user.id !== user.id) {
+      throw new Forbidden();
+    }
+    try {
+      await prisma.user.delete({
+        where: { id: user.id },
+      });
+
+      const deletePromises = user.Playlist.map((playlist) => {
+        const thumbnailKey = extractS3Key(playlist.thumbnailUrl);
+        if (thumbnailKey) {
+          return deleteFromS3(process.env.S3_BUCKET_NAME, thumbnailKey);
+        }
+        return Promise.resolve();
+      });
+      await Promise.all(deletePromises);
+    } catch (error) {
+      console.error("error deleting user");
+      throw new Unknown();
+    }
+
+    return;
   });
